7cdcb3821bbaba339b6241417dba5dd26d93b68a
builiu0405 <buithibichlieu04052004@gmail.com>
2026-03-19 10:19:51 +0700

    first commit

---
+++ src/test/java/com/example/BuiThiBichLieu/BuiThiBichLieuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuiThiBichLieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuiThiBichLieuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
